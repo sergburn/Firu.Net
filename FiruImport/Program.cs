a4cd9b3959513692b853494a19275124b44c89db
Sergey Burnevsky <sergey.burnevsky@gmail.com>
2011-10-30 23:33:48 +0200

    first commit

---
+++ FiruImport/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Firu
+{
+    class Import
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
