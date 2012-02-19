b5a08dd5e75f698b0ae3576ff9f7c40b48d11af3
Sergey Burnevsky <sergey.burnevsky@gmail.com>
2012-02-19 13:49:25 +0200

    added project for WP

---
+++ FiruPhone/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace FiruPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Конструктор
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Задайте для контекста данных элемента управления listbox пример данных
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // Загрузка данных для элементов ViewModel
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+    }
+}
